85c1a631fda51e896a5193d0b8701f20722cc7e3
ParkJunsu <53895687+steve7867@users.noreply.github.com>
2021-03-21 22:56:01 +0900

    Add function to determine whether year is leap.

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -4,19 +4,27 @@ import java.util.Scanner;
 
 public class Calendar {
 	private static final int[] MAX_DAYS = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-
-	public int getMaxDaysOfMonth(int month) {
-		return MAX_DAYS[month];
+	private static final int[] LEAP_MAX_DAYS = { 0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	
+	public boolean isLeapYear(int year) {
+		if (year % 400 == 0) return true;
+		if (year % 100 == 0) return false;
+		if (year % 4 == 0) return true;
+		return false;
+	}
+	
+	public int getMaxDaysOfMonth(int year, int month) {
+		return isLeapYear(year) ? LEAP_MAX_DAYS[month] : MAX_DAYS[month];
 	}
 
 	public void printCalendar(int year, int month) {
-		System.out.printf("     <<%4d년%3d월>>\n", year, month);
-		System.out.println(" SU  MO  TU  WE  TH  FR  SA");
-		System.out.println("---------------------------");
+		System.out.printf("   <<%4d년%3d월>>\n", year, month);
+		System.out.println(" SU MO TU WE TH FR SA");
+		System.out.println("---------------------");
 		
-		int maxDays = getMaxDaysOfMonth(month);
+		int maxDays = getMaxDaysOfMonth(year, month);
 		for (int i = 1; i <= maxDays; i++) {
-			System.out.printf("%3d ", i);
+			System.out.printf("%3d", i);
 			if (i % 7 == 0 || i == maxDays)
 				System.out.println();
 		}
--- src/honux/calendar/Prompt.java
+++ src/honux/calendar/Prompt.java
@@ -3,7 +3,6 @@ package honux.calendar;
 import java.util.Scanner;
 
 public class Prompt {
-	private static final String PROMPT = "cal> ";
 
 	public void runPrompt() {
 
@@ -11,29 +10,22 @@ public class Prompt {
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
 
-		String res = "";
-		System.out.println("월을 입력하세요.");
 		while (true) {
-			System.out.print(PROMPT);
+			System.out.println("년을 입력하세요.");
+			System.out.print("YEAR> ");
+			int year = scanner.nextInt();
+			
+			System.out.println("월을 입력하세요.");
+			System.out.print("MONTH> ");
 			int month = scanner.nextInt();
 			if (month < 0)
 				break;
 			else if (month == 0 || month > 12)
 				continue;
-			res += String.format("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
+			
+			cal.printCalendar(year, month);
 		}
-		System.out.println(res);
-
-		while (true) {
-			System.out.println("몇 월 달력을 출력할까요?");
-			System.out.print(PROMPT);
-			int month = scanner.nextInt();
-			if (month < 0)
-				break;
-			else if (month == 0 || month > 12)
-				continue;
-			cal.printCalendar(2021, month);
-		}
+		
 		System.out.println("Bye~");
 		scanner.close();
 	}
