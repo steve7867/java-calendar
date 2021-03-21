8bfd78a21d8dc7e07a8799703b7ac79748a9dd9f
ParkJunsu <53895687+steve7867@users.noreply.github.com>
2021-03-21 17:39:52 +0900

    Update Calendar main()

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -2,6 +2,14 @@ package honux.calendar;
 
 public class Calendar {
 	public static void main(String[] args) {
-		System.out.println("Hello, Calendar");
+		System.out.println("일 월 화 수 목 금 토");
+		System.out.println("--------------------");
+		int day = 1;
+		for (int i = 0; i < 4; i++) {
+			for (int j = 0; j < 7; j++) {
+				System.out.printf("%2d ", day++);
+			}
+			System.out.println();
+		}
 	}
 }
