ec093fc9b3d1ac073f802697d1a1a39f7fa3e3af
ParkJunsu <53895687+steve7867@users.noreply.github.com>
2021-03-21 18:41:01 +0900

    Add reapeat function

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -3,12 +3,12 @@ package honux.calendar;
 import java.util.Scanner;
 
 public class Calendar {
-	private static final int[] MAX_DAYS = {0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-	
+	private static final int[] MAX_DAYS = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
 	public int getMaxDaysOfMonth(int month) {
 		return MAX_DAYS[month];
 	}
-	
+
 	public void printSampleCalendar() {
 		System.out.println("일 월 화 수 목 금 토");
 		System.out.println("--------------------");
@@ -21,20 +21,26 @@ public class Calendar {
 		}
 		System.out.println();
 	}
-	
+
 	public static void main(String[] args) {
-		
-		
-		//숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
+
+		// 숫자를 입력받아 해당하는 달의 최대 일수를 출력하는 프로그램
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
-		System.out.println("달을 입력하세요.");
-		int month = scanner.nextInt();
-		System.out.printf("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
-		System.out.println();
 		
+		System.out.println("반복횟수를 입력하세요.");
+		int n = scanner.nextInt();
+		
+		String res = "";
+		System.out.println("월을 입력하세요.");
+		for (int i = 0; i < n; i++) {
+			int month = scanner.nextInt();
+			res += String.format("%d월은 %d일까지 있습니다.\n", month, cal.getMaxDaysOfMonth(month));
+		}
+		System.out.println(res);
+
 		cal.printSampleCalendar();
-		
+
 		scanner.close();
 	}
 }
